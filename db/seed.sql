139db2c0c3e1afa2cf496b0f27a0a78ba73e4dbd
dashaman <hang.andy.t@gmail.com>
2017-04-30 14:31:17 -0500

    initial

---
+++ db/seed.sql
@@ -0,0 +1,7 @@
+INSERT INTO burgers (
+    burger_name,
+    orderDate
+) VALUES
+("Bacon Cheese Burger", "20170430"),
+("Double Cheese Burger", "20170430"),
+("Smoked Hickory Cheese Burger", "20170430");
